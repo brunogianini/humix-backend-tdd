f803e7e7853766d4e5c8d0ee785518aafea1b60f
Bruno Gianini <blgianini@gmail.com>
2025-08-31 11:29:15 -0300

    fix: corrigindo lógica da criação de álbum

--- src/services/AlbumService.ts
+++ src/services/AlbumService.ts
@@ -8,11 +8,12 @@ export class AlbumService {
     constructor(private albumRepository: AlbumRepository) {}
 
     async criarAlbum(data: CriarAlbumRequestDTO): Promise<Album>{
-        const albumExiste = await this.albumRepository.findByName(data.nome)
+        const spotifyPesquisa = await searchAlbum(data.nome, data.banda)
+        const albumExiste = await this.albumRepository.findByName(spotifyPesquisa.nome)
 
         if(albumExiste) throw new Error('Este álbum já foi criado')
 
-        const spotifyPesquisa = await searchAlbum(data.nome, data.banda)
+        
         const album: CriarAlbumDTO = {nome: spotifyPesquisa.nome, banda: spotifyPesquisa.banda, capa: spotifyPesquisa.capa, ano: new Date(spotifyPesquisa.ano)}
 
         return this.albumRepository.create(album)
